d0f3bf194cbd1044b7491fc199334ead7508c9c4
Asghar Waheed <arhumkhattak05@gmail.com>
2023-02-14 09:42:17 +0500

    added project

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+   
+   const Sdata=[
+    {
+        imgsrc:"../images/Dark.jpg",
+        title:"A Netflix Orignal Series",
+        sname:"DARK",
+        link:"https://www.netflix.com/pk/title/80100172"
+    },
+    {
+        imgsrc:"../images/extra.jpg",
+        title:"A Netflix Orignal Series",
+        sname:"Extra Curricular",
+        link:"https://www.netflix.com/pk/title/80990668"
+    },
+    {
+        imgsrc:"../images/stranger.jpg",
+        title:"A Netflix Orignal Series",
+        sname:"Stranger Things",
+        link:"https://www.imdb.com/title/tt4574334/"
+    },
+    {
+        imgsrc:"../images/vampire.jpg",
+        title:"A Netflix Orignal Series",
+        sname:"The Vampire Diaries",
+        link:"https://www.imdb.com/title/tt4574334/"
+    },
+    {
+        imgsrc:"../images/first.jpg",
+        title:"A Netflix Orignal Series",
+        sname:"My First First Love",
+        link:"https://www.netflix.com/pk/title/81026700"
+    }
+]
+
+export default Sdata;
